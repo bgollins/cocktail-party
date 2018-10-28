12c3bd84ea453f49f193784edad349fb0180d136
Brittany Gollins <brittany.gollins@gmail.com>
2018-10-28 12:35:27 -0700

    create initial files for Week 13 individual project

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+//Biz logic copied from ping pong assignment
+function pongMachine(numberAbs) {
+  var numberArray = []
+  for (var index = 1; index <= numberAbs; index += 1) {
+
+    if (index % 15 == 0) {
+      numberArray.push("pingpong")
+    } else if (index % 5 == 0) {
+      numberArray.push("pong")
+    } else if (index % 3 == 0) {
+      numberArray.push("ping")
+    } else {
+      numberArray.push(index)
+    }
+  }
+  return numberArray;
+};
+
+//UI logic copied from pingpong assignment
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+    var numberAbs = Math.abs(number);
+    var numberArray = pongMachine(numberAbs);
+    numberArray.forEach(function(x) {
+      $('#resultList').append("<li>" + x + "</li>");
+    });
+    $("#result").show(numberArray);
+  });
+});
